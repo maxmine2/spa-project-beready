0fbb0997d3949081ec3fb9e333a4e7f6cc85e270
maxmine2 <mbudko2@gmail.com>
2021-01-11 23:24:38 +0700

    NC-04 Commit
    This update touches settings, adds skipped for a while features

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+#*     BeReady, version nr-04
+#*  Copyright (C) 2021  Max Budko
 import datetime
 import json
 import os
@@ -7,6 +10,8 @@ from flask import Flask, redirect, session, url_for, request
 from werkzeug.utils import html
 
 import db
+
+MAINSETS = json.load('sets.json')
 
 app = Flask(__name__, static_folder='static')
 
@@ -41,7 +46,13 @@ def textid_gen(): # * Не проверено
     textid = ""
     for _ in range(randint(5, 7)):
         textid += TEXTABC['consonants'][randint(0, len(TEXTABC['consonants']))] + TEXTABC['vowels'][randint(0, len(TEXTABC['vowels']))]
-    return textid 
+    return textid
+
+
+def nosession():
+    session_id = session_gen()
+    db.Sessions.add_one(session_id, False)
+    return json.dumps({"is_logged_in": False, "login_page": "/static/login.html", "new_session":  session_id}, indent=4)
 
 @app.route('/')
 def main_page_load():
@@ -53,8 +64,14 @@ def main_page_load():
 
 @app.route('/text')
 def texts():
-    # TODO Сделать страницу с текстами
-    pass
+    all_texts = db.Texts.get_all()
+    dict_data = {
+        f"text{text[0]}": {
+            "id": text[0],
+            "title": text[2],
+        } for text in all_texts
+    }
+    return json.dumps(dict_data)
 
 @app.route('/text/<id>')
 def text_get(id):
@@ -64,14 +81,12 @@ def text_get(id):
 
 @app.route('/app')
 def admin_application():
-    requestdata = request.text
+    requestdata = request.get_json()
     requestdata = json.loads(requestdata)
     if requestdata['session'] is "-NoSession-":
-        session_id = session_gen()
-        db.Sessions.add_one(session_id, False)
-        return json.dumps({"is_logged_in": False, "login_page": "/static/login.html", "new_session":  session_id}, indent=4)
+        return nosession()
     else:
-        requestdata = request.text
+        requestdata = request.get_json()
         requestdata = json.loads(requestdata)
         if db.Sessions.is_exists_session(session):
             if db.Sessions.is_validated_session(session):
@@ -81,22 +96,40 @@ def admin_application():
         else:
             return json.dumps({"error": "invalid_session"})
 
+@app.route('/internal/login', methods=['POST'])
+def login_application():
+    requestdata = request.get_json()
+    requestdata = json.loads(requestdata)
+    if requestdata['session'] is "-NoSession-":
+        return nosession()
+    else:
+        if db.Sessions.is_exists_session(session):
+            if db.Sessions.is_valid_session(session):
+                return json.dumps({'is_logged_in': True, 'app_page': '/static/app.html'})
+            else:
+                if requestdata['psswrd'] == MAINSETS['password']:
+                    db.Sessions.validate(requestdata['session'])
+                    return json.dumps({'is_logged_in': True, 'app_page': '/static/app.html'})
+                else:
+                    return json.dumps({'problem':'login_failed', 'reason':'psswrd-incorrect'})
+        else:
+            return json.dumps({'problem':'invalid_session'})
+       
+
 @app.route('/internal/addtext', methods=["POST"])
 def text_add_application():
     requestdata = request.get_json()
     requestdata = json.loads(requestdata)
     if requestdata['session'] is "-NoSession-":
-        session_id = session_gen()
-        db.Sessions.add_one(session_id, False)
-        return json.dumps({"status":"lg-req", "is_logged_in": False, "login_page": "/static/login.html"})
+        return nosession()
     if db.Sessions.is_exists_sessions(session):
         if db.Session.is_validated_session(session):
             name = requestdata['text_name']
             title = requestdata['text']['title'] if requestdata['text']['title'] != "" else TITLE_PLACEHOLDER
             text = requestdata['text']['text']
             js = requestdata['text']['js'] if requestdata['text']['js'] != "" else None
-            # TODO: Доделать сохранение в файл и отправку запроса на БД.
-            # ! В процессе, лучше не запускать =)
+            # * TODO: Доделать сохранение в файл и отправку запроса на БД.
+            # * Не проверено
             textnames = textid_gen()
             f = open(f'static/title_{textnames}.html', 'w+'), open(f'static/text_{textnames}.html', 'w+'), open(f'static/{textnames}.js', 'w+')
             for i, elem in range(3), [title, text, js]:
@@ -105,10 +138,6 @@ def text_add_application():
             del f
             db.Text.add_one(textnames, name)
             return {"status": "scs"}
-            
-
-
-
         else:
             return json.dumps({'is_logged_in': False, 'login_page': '/static/login.html'})
     else:
