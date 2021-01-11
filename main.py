195212b452d329352a97be1c9485bb9f97d3a240
maxmine2 <mbudko2@gmail.com>
2021-01-11 20:47:12 +0700

    A little changes
    Textid_gen() now works, added "indent=4" in some JSON forming functions.
    Also some functions are modified, and now returning a bit more relevant data.
    
    Also the function that saves texts is now working.

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ app.config['SECRET_KEY'] = key
 
 ABC = """1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"""
 TEXTABC = {
-    "vowels": "aeyui",
-    "consonants":"dfnlkbn"
+    "vowels": "aeyuio",
+    "consonants":"dfnlkbnv"
 }
 
 TITLE_PLACEHOLDER = ""
@@ -37,10 +37,11 @@ def session_gen():
         session += ABC[randint(0, len(ABC))]
     return session
 
-def textid_gen():
+def textid_gen(): # * Не проверено
     textid = ""
     for _ in range(randint(5, 7)):
-        textid += TEXTABC
+        textid += TEXTABC['consonants'][randint(0, len(TEXTABC['consonants']))] + TEXTABC['vowels'][randint(0, len(TEXTABC['vowels']))]
+    return textid 
 
 @app.route('/')
 def main_page_load():
@@ -48,7 +49,7 @@ def main_page_load():
     # d = open.readlines()
     # o.close()
     # return d
-    return 
+    return
 
 @app.route('/text')
 def texts():
@@ -74,9 +75,9 @@ def admin_application():
         requestdata = json.loads(requestdata)
         if db.Sessions.is_exists_session(session):
             if db.Sessions.is_validated_session(session):
-                return json.dumps({"is_logged_in": True, "app_page": "/static/app.html"})
+                return json.dumps({"is_logged_in": True, "app_page": "/static/app.html"}, indent=4)
             else:
-                return json.dumps({"is_logged_in": False, "login_page": "/static/login.html"})
+                return json.dumps({"is_logged_in": False, "login_page": "/static/login.html"}, indent=4)
         else:
             return json.dumps({"error": "invalid_session"})
 
@@ -87,13 +88,26 @@ def text_add_application():
     if requestdata['session'] is "-NoSession-":
         session_id = session_gen()
         db.Sessions.add_one(session_id, False)
-        return json.dumps({"is_logged_in": True, "login_page": "/static/login.html"})
+        return json.dumps({"status":"lg-req", "is_logged_in": False, "login_page": "/static/login.html"})
     if db.Sessions.is_exists_sessions(session):
         if db.Session.is_validated_session(session):
+            name = requestdata['text_name']
             title = requestdata['text']['title'] if requestdata['text']['title'] != "" else TITLE_PLACEHOLDER
             text = requestdata['text']['text']
             js = requestdata['text']['js'] if requestdata['text']['js'] != "" else None
+            # TODO: Доделать сохранение в файл и отправку запроса на БД.
+            # ! В процессе, лучше не запускать =)
+            textnames = textid_gen()
+            f = open(f'static/title_{textnames}.html', 'w+'), open(f'static/text_{textnames}.html', 'w+'), open(f'static/{textnames}.js', 'w+')
+            for i, elem in range(3), [title, text, js]:
+                f[i].writelines(elem)
+                f.close()
+            del f
+            db.Text.add_one(textnames, name)
+            return {"status": "scs"}
             
+
+
 
         else:
             return json.dumps({'is_logged_in': False, 'login_page': '/static/login.html'})
