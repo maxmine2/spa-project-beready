9afc0fd2baa1612d8e1c91e5d2192272169bb56e
maxmine2 <mbudko2@gmail.com>
2021-01-30 15:33:45 +0700

    Little improvements

--- static/core.js
+++ static/core.js
@@ -8,13 +8,15 @@ var data =
 {
     title: "",
     body: "",
-    jscode:"",
+    link: "",
+    jscode:""
 };
 
 var page = 
 {
     title: document.getElementById("title"),
-    body: document.getElementById("body")
+    body: document.getElementById("body"),
+    jscode: document.getElementById("jscode")
 }
 
 OnLoad();
@@ -82,7 +84,7 @@ function SendRequest(query, param)
             else if (query == 'app') preprocessJSONData('app', xhr.responseText, link, -1)
             
             else if (query == 'static') LoadData(param, xhr.responseText, link)
-        else 
+        else
         {
             alert("Loading error! Please, go to the main page and try again later.");
             console.log(xhr.status + ":" + xhr.statusText);
@@ -100,7 +102,45 @@ function preprocessJSONData(request_category, response, link, param)
     var readydata = JSON.parse(response);
     if (request_category == 'texts') {
         body = "<h1>All texts</h1>\n"
-        for (var i = 0; i < readydata.length; i++) body += "<a href='/text/" + readydata["text" + i][id] + "' class='link link-internal'><p class='text-regular'>Text "+ readydata["text" + i][id] + "</p><p class='text-title'> "
+        for (var i = 0; i < readydata.length; i++) body += "<div class=''><a href='/text/" + readydata["text" + i]["id"] + "' class='link link-internal link-text'><p class='text-regular'>Text "+ readydata["text" + i]["id"] + ".</p><p class='texts-text-title'> " + readydata["text" + i]["text"] + "</p></a></div>"
+        data = {
+            title: 'All texts — BeReady',
+            body: body,
+            link: link,
+            jscode: "",
+        }
+        UpdatePage();
     }
-        
+    else if (request_category == 'text') {
+        SendRequest(request_category['title'].split('/')[1], request_category['title'].split('/')[2]);
+        SendRequest(request_category['text'].split('/')[1], request_category['text'].split('/')[2]);
+        SendRequest(request_category['js'].split('/')[1], request_category['js'].split('/')[2]);
+    }
+
+    else if (request_category == 'app') {
+        SendRequest()
+    }
+
+}
+
+function LoadData(param, response, link)
+{
+    if (param.substr(0, 4) == 'titl') data.title = response
+    else if (param.substr(0, 4) == 'text') data.text = response
+    else if (param.substr(length(param) - 4) == '.js') data.jscode = response;
+    data.link = ;
+    UpdatePage();
+}
+
+function UpdatePage()
+{
+    page.title.innerText = data.title;
+    page.body.innerHTML = data.body;
+    page.jscode.innerText = data.jscode;
+
+    document.title = data.title;
+    window.history.pushState(data.body, data.title, data.link);
+
+    InitLinks();
+    
 }
